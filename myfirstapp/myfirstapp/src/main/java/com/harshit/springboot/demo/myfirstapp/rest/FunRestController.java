1a86d8f01b934c61e429ce3ec7bb1c6cd0ba5fc4
Harshit Kuchhal <harshitkuchhal1997@gmail.com>
2023-04-09 22:08:29 +0530

    Injected Custom application properties

--- myfirstapp/myfirstapp/src/main/java/com/harshit/springboot/demo/myfirstapp/rest/FunRestController.java
+++ myfirstapp/myfirstapp/src/main/java/com/harshit/springboot/demo/myfirstapp/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.harshit.springboot.demo.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach and team name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoints for "team and coach info"
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: "+coachName+", Team name: "+teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
